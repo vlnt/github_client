2597ea6953ba3e42cd767d98def6ff1e4d4229d0
Valentyn Mirosh <vlnt@ua.fm>
2023-12-28 02:44:19 +0200

    oauth

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+const githubClientId = '5a28c94d118a1d119a1d';
+const githubClientSecret = 'd1a7507eeb390ea274589570ada11cc12a3522ef';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
